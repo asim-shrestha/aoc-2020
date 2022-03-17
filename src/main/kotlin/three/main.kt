b14b27f36a681e535d0af4fc4d81f45858aeb59f
Asim Shrestha <asimshre@amazon.com>
2022-03-17 16:20:35 -0700

    feat: finish aoc 3

--- src/main/kotlin/three/main.kt
+++ src/main/kotlin/three/main.kt
@@ -1,37 +1,63 @@
 package three
 
 import java.io.File
+import java.math.BigInteger
+fun Long.toBigInteger() = BigInteger.valueOf(this)
 
 fun main() {
-    val input = File("src/main/kotlin/two/input.txt").readLines()
+    val input = File("src/main/kotlin/three/input.txt").readLines()
+    println(TobogganTrajectory(TreePattern.fromInputList(input)).travelToBottom())
+
+    val values =
+        listOf(
+        Pair(1, 1),
+        Pair(3, 1),
+        Pair(5, 1),
+        Pair(7, 1),
+        Pair(1, 2)
+    ).map { TobogganTrajectory(TreePattern.fromInputList(input)).travelToBottom(it.first, it.second) }
+    println(values)
+    println(values.fold (BigInteger.ONE) {  sum, element -> sum.multiply(element.toBigInteger()) })
 }
 
 data class TreePattern(val treeMatrix: List<List<Boolean>>) {
     companion object Builder {
         fun fromInputList(inputList: List<String>): TreePattern {
+            require(inputList.isNotEmpty() && inputList.none { it == "" })
+
             return TreePattern(inputList.map { it.map { char ->
                 when(char) {
                     '.' -> false
                     '#' -> true
-                    else -> throw IllegalArgumentException()
+                    else -> {print(char); throw IllegalArgumentException()}
                 }
             }})
         }
     }
 }
 
-class TobbaganTrajectory(private val treePattern: TreePattern) {
+class TobogganTrajectory(private val treePattern: TreePattern) {
     private var currRow = 0
     private var currCol = 0
 
     val isAtTree: Boolean
-        get() = treePattern.treeMatrix[0][0]
+        get() = treePattern.treeMatrix[currRow][currCol]
 
-    fun hasReachedBottom() = currRow >= treePattern.treeMatrix.size
+    fun isAtBottom() = currRow >= treePattern.treeMatrix.size - 1
 
     fun move(dr: Int, dc: Int) {
         currRow = (treePattern.treeMatrix.size - 1).coerceAtMost(currRow + dr)
-        currCol = (treePattern.treeMatrix[0].size + dc) % treePattern.treeMatrix[0].size
+        currCol = (currCol + dc) % treePattern.treeMatrix[0].size
+    }
+
+    // Returns number of trees found in decent to bottom
+    fun travelToBottom(right: Int = 3, left: Int = 1): Int {
+        var numTrees = 0
+        while(!isAtBottom()) {
+            move(left, right)
+            numTrees += if(isAtTree) 1 else 0
+        }
+        return numTrees
     }
 }
 
--- src/test/kotlin/three/MainTest.kt
+++ src/test/kotlin/three/MainTest.kt
@@ -2,6 +2,7 @@ package three
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class MainTest : DescribeSpec({
     val sampleTreePattern = listOf(
@@ -19,6 +20,13 @@ class MainTest : DescribeSpec({
     )
 
     describe("Generate tree pattern") {
+        it("Fails on empty input") {
+            listOf(listOf(), listOf("")).forEach{
+                shouldThrow<IllegalArgumentException> {
+                    TreePattern.fromInputList(it)
+                }
+            }
+        }
         it("Exception if pattern contains non '.', '#' characters") {
             shouldThrow<IllegalArgumentException> {
                 TreePattern.fromInputList(listOf("..#123."))
@@ -27,6 +35,54 @@ class MainTest : DescribeSpec({
 
         it("Succeeds with sample input") {
             TreePattern.fromInputList(sampleTreePattern)
+        }
+    }
+
+    describe("Basic is at Tree") {
+        it("Tree only pattern is at tree") {
+            TobogganTrajectory(TreePattern.fromInputList(listOf("#"))).isAtTree shouldBe true
+        }
+
+        it("Snow only pattern is NOT at tree") {
+            TobogganTrajectory(TreePattern.fromInputList(listOf("."))).isAtTree shouldBe false
+        }
+    }
+
+    describe("Travel to bottom") {
+        it("7 trees in first sample input decent") {
+           val tt = TobogganTrajectory(TreePattern.fromInputList(sampleTreePattern))
+            tt.travelToBottom() shouldBe 7
+            tt.isAtBottom() shouldBe true
+        }
+
+        it("right 1 slope test") {
+            val tt = TobogganTrajectory(TreePattern.fromInputList(sampleTreePattern))
+            tt.travelToBottom(1) shouldBe 2
+            tt.isAtBottom() shouldBe true
+        }
+
+        it("right 3 slope test") {
+            val tt = TobogganTrajectory(TreePattern.fromInputList(sampleTreePattern))
+            tt.travelToBottom(3) shouldBe 7
+            tt.isAtBottom() shouldBe true
+        }
+
+        it("right 5 slope test") {
+            val tt = TobogganTrajectory(TreePattern.fromInputList(sampleTreePattern))
+            tt.travelToBottom(5) shouldBe 3
+            tt.isAtBottom() shouldBe true
+        }
+
+        it("right 7 slope test") {
+            val tt = TobogganTrajectory(TreePattern.fromInputList(sampleTreePattern))
+            tt.travelToBottom(7) shouldBe 4
+            tt.isAtBottom() shouldBe true
+        }
+
+        it("right 1 down 2 slope test") {
+            val tt = TobogganTrajectory(TreePattern.fromInputList(sampleTreePattern))
+            tt.travelToBottom(1,2) shouldBe 2
+            tt.isAtBottom() shouldBe true
         }
     }
 })
